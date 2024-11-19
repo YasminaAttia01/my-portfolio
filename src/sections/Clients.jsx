ccd962f1ee6e5845ba079d1d4aba869c6cc3aca0
YasminaAttia01 <attiayasmina01@gmail.com>
2024-11-19 16:53:31 +0100

    adding the three js part avatar and work experience !

--- src/sections/Clients.jsx
+++ src/sections/Clients.jsx
@@ -10,12 +10,19 @@ const Clients = () => {
                     <div>
                         <p className='text-white font-light'>{review}</p>
                         <div className='client-content'>
-                            <div className='flex gap-0'>
+                            <div className='flex gap-3'>
 
                                 <img src={img} alt={name} className="w-12 h-12 rounded-full"/>
                                 <div className="flex flex-col">
-                                    <p className="font-semibold text-white">{position}</p>
+                                    <p className="font-semibold text-white-800">{name}</p>
+                                    <p className="text-white-500 md:text-base text-sm font-light"> {position}</p>
                                 </div>
+                            </div>
+                            <div className="flex self-end items-center gap-2">
+                                {Array.from({length: 5}).map((_, index) => (
+                                    <img key={index} src="/assets/star.png" alt="star"className="w-5 h-5"/>
+                                ))}
+
                             </div>
                         </div>
                     </div>
